509ba205567bf3990c2c6ecbc30202c402ecab3f
siddhesh326464 <parabsiddhesh60@gmail.com>
2026-06-07 13:26:14 +0530

    adding hybride search in this

--- apps/services/retrieval/qdrant_service.py
+++ apps/services/retrieval/qdrant_service.py
@@ -42,54 +42,54 @@ def search_enterprise_knowledge(query:str,limit:int = settings.TOP_K):
         return []
 
 
-# def search_enterprise_knowledge(query:str,limit:int = settings.TOP_K):
-#     """
-#     Performs a high-precision search in the enterprise knowledge base.
-#     Uses the modern query_points interface.
-#     """
-#     try:
-#         sparse_query = query
-#         query = embed_query(query)
-#         sparse_query = embed_sparse_query(sparse_query)
+def search_enterprise_knowledge(query:str,limit:int = settings.TOP_K):
+    """
+    Performs a high-precision search in the enterprise knowledge base.
+    Uses the modern query_points interface.
+    """
+    try:
+        sparse_query = query
+        query = embed_query(query)
+        sparse_query = embed_sparse_query(sparse_query)
 
-#         sparse_vectores = models.SparseVectors(
-#             indices=sparse_query["indices"],
-#             values=sparse_query["values"]   
-#         )
+        sparse_vectores = models.SparseVectors(
+            indices=sparse_query["indices"],
+            values=sparse_query["values"]   
+        )
 
-#         response = qdrant_client.query_points(
-#             collection_name = settings.QDRANT_COLLECTION,
-#             prefetch=[
-#                 models.Prefetch(
-#                     query = query,
-#                     using = "dense",
-#                     limit=limit * 2
-#                 ),
+        response = qdrant_client.query_points(
+            collection_name = settings.QDRANT_COLLECTION,
+            prefetch=[
+                models.Prefetch(
+                    query = query,
+                    using = "dense",
+                    limit=limit * 2
+                ),
 
-#                 #sparse retrieval can be added in the future as needed
+                #sparse retrieval can be added in the future as needed
 
-#                 models.Prefetch(
-#                     query = sparse_vectores,
-#                     using = "sparse",
-#                     limit=limit * 2
-#                 )
-#             ],
-#             query = models.FusionQuery(
-#                 fusion=models.Fusion.RRF
-#             ),
-#             limit=limit,
-#             with_payload=True,
-#         )
+                models.Prefetch(
+                    query = sparse_vectores,
+                    using = "sparse",
+                    limit=limit * 2
+                )
+            ],
+            query = models.FusionQuery(
+                fusion=models.Fusion.RRF
+            ),
+            limit=limit,
+            with_payload=True,
+        )
 
-#         results = []
-#         for res in response.points:
-#             results.append({
-#                 "content": res.payload.get("text", ""),
-#                 "source": res.payload.get("source", "Unknown"),
-#                 "score": res.score
-#             })
+        results = []
+        for res in response.points:
+            results.append({
+                "content": res.payload.get("text", ""),
+                "source": res.payload.get("source", "Unknown"),
+                "score": res.score
+            })
 
-#         return results
-#     except Exception as e:
-#         logfire.error(f"❌ Qdrant Search Failed: {e}")
-#         return []
+        return results
+    except Exception as e:
+        logfire.error(f"❌ Qdrant Search Failed: {e}")
+        return []
